1eb8dc3c8361ca1a60a05ad45ed4f291adf813ac
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-11 11:49:27 +0200

    ajouter le schéma de modèle Recette

--- models/Recette.js
+++ models/Recette.js
@@ -1,2 +1,25 @@
 const mongoose = require('mongoose');
 
+// Création du schéma pour une recette
+const recetteSchema = new mongoose.Schema({
+    titre: {
+        type: String,
+        required: true
+    },
+    ingredients: {
+        type: [String],
+        required: true
+    },
+    etapes: {
+        type: String,
+        required: true
+    },
+    auteur: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
