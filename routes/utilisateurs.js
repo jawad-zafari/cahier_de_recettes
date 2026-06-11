d87904cfbeefebdc75c5b03ce8175cd0591d4926
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-11 16:23:28 +0200

    ajouter la gestion des erreurs lors de la connexion de l'utilisateur

--- routes/utilisateurs.js
+++ routes/utilisateurs.js
@@ -38,7 +38,14 @@ router.post('/inscription', async (req, res) => {
 // Route pour la connexion
 router.post('/connexion', async (req, res) => {
     try {
+        // Trouver l'utilisateur par son email
+        const utilisateur = await Utilisateur.findOne({ email: req.body.email });
+        if (!utilisateur) {
+            return res.status(400).json({ message: 'Email ou mot de passe incorrect.' });
+        }
+
         } catch (erreur) {
+        res.status(500).json({ message: erreur.message });
     }
 });
 
