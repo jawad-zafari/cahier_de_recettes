34d917b7cdfd3b81226fc1a964be3fdfb168c026
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-11 16:32:30 +0200

    ajouter la route pour les utilisateurs dans le serveur

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
 
 app.use('/recettes', recettesRoutes);
+app.use('/utilisateurs', utilisateursRoutes);
 
 // URL de connexion à la base de données locale
 const dbURL = 'mongodb://127.0.0.1:27017/cahier_de_recettes';
