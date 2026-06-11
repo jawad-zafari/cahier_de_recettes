98c5460781022de69d352c47e7037d7146126569
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-11 16:17:58 +0200

    ajouter la clé secrète pour le token

--- routes/utilisateurs.js
+++ routes/utilisateurs.js
@@ -4,3 +4,6 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const Utilisateur = require('../models/Utilisateur');
 
+// Clé secrète pour le token (à garder secrète dans un vrai projet)
+const CLE_SECRETE = 'ma_cle_super_secrete_123';
+
