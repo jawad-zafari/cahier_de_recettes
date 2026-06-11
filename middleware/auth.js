536673d9f9eee83c24c01956978f4bc3de35d68e
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-11 21:01:04 +0200

    ajouter la gestion des erreurs pour les tokens invalides ou expirés

--- middleware/auth.js
+++ middleware/auth.js
@@ -19,8 +19,8 @@ module.exports = (req, res, next) => {
         // Ajouter les informations de l'utilisateur à la requête
         req.utilisateur = tokenDecode;
         
-        // Passer à la suite (la route demandée)
         next();
     } catch (erreur) {
+        res.status(401).json({ message: 'Token invalide ou expiré.' });
     }
 };
