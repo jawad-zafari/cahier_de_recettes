b32ebefe538127b710ccab3bf85eb556c4273ec1
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-11 16:10:59 +0200

    ajouter le schéma Utilisateur avec mongoose

--- models/Utilisateur.js
+++ models/Utilisateur.js
@@ -1,2 +1,22 @@
 const mongoose = require('mongoose');
 
+// Création du schéma pour un utilisateur
+const utilisateurSchema = new mongoose.Schema({
+    nom: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true // L'email doit être unique pour chaque utilisateur
+    },
+    motDePasse: {
+        type: String,
+        required: true
+    },
+    dateCreation: {
+        type: Date,
+        default: Date.now
+    }
+});
