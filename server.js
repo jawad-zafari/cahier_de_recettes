dc6c0e5c226e6dab090ac09beafba05b61e3b6d5
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-11 16:33:34 +0200

    ajouter un commentaire

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
 
 app.use('/recettes', recettesRoutes);
+// Utilisation des routes utilisateurs
 app.use('/utilisateurs', utilisateursRoutes);
 
 // URL de connexion à la base de données locale
