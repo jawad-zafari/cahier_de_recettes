bb0a01c74d7a3755c735b99f327e0bd4d3258d04
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-11 20:55:43 +0200

    ajouter la récupération du token

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,6 +3,9 @@ const CLE_SECRETE = process.env.CLE_SECRETE;
 
 module.exports = (req, res, next) => {
     try {
+        // Récupérer le token depuis l'en-tête de la requête
+        const enTeteAuth = req.headers.authorization;
+        
         } catch (erreur) {
     }
 };
