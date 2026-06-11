a8fbf2f1f9706e3aa9cecb783b0972c1d37edd74
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-11 20:56:49 +0200

    ajouter la vérification  pour l'accès

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,6 +6,10 @@ module.exports = (req, res, next) => {
         // Récupérer le token depuis l'en-tête de la requête
         const enTeteAuth = req.headers.authorization;
         
+        if (!enTeteAuth) {
+            return res.status(401).json({ message: 'Accès refusé. Veuillez vous connecter.' });
+        }
+
         } catch (erreur) {
     }
 };
