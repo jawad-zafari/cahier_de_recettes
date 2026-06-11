3458d01769fa1506c50763913cea8bd608d8f69b
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-11 11:33:17 +0200

    ajouter la logique de démarrage du serveur et la route de base

--- server.js
+++ server.js
@@ -4,3 +4,10 @@ const app = express();
 
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+    res.send('Bonjour! Le serveur a démarré avec succès.');
+});
+
+app.listen(PORT, () => {
+    console.log("Le serveur fonctionne sur le port : 3000");
+});
