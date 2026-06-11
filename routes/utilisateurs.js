fd092903636e8564be496d4044952aa24967cfff
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-11 16:22:23 +0200

    ajouter la sauvegarde de l'utilisateur dans la base de données

--- routes/utilisateurs.js
+++ routes/utilisateurs.js
@@ -27,7 +27,10 @@ router.post('/inscription', async (req, res) => {
             motDePasse: motDePasseCrypte
         });
 
-        } catch (erreur) {
+        // Sauvegarder dans la base de données
+        await nouvelUtilisateur.save();
+        res.status(201).json({ message: 'Utilisateur créé avec succès !' });
+    } catch (erreur) {
         res.status(500).json({ message: erreur.message });
     }
 });
