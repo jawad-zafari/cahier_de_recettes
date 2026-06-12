d5c5dd109fd2d49062eeffbd9330a08c8fa0ea5d
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-12 10:47:05 +0200

    Ajouter les routes pour la gestion des recettes

--- routes/recettes.js
+++ routes/recettes.js
@@ -3,3 +3,11 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const recetteController = require('../controllers/recetteController');
 
+router.post('/', auth, recetteController.creerRecette);
+router.get('/', recetteController.obtenirRecettes);
+router.get('/:id', recetteController.obtenirRecetteParId);
+router.patch('/:id', auth, recetteController.modifierRecette);
+router.delete('/:id', auth, recetteController.supprimerRecette);
+router.post('/:id/commentaires', recetteController.ajouterCommentaire);
+
+module.exports = router;
