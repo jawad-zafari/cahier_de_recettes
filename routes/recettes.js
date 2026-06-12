cd8814043253e5b512ec412ae9f49e256d8e0126
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-12 10:46:09 +0200

    Ajouter l'importation du contrôleur de recettes

--- routes/recettes.js
+++ routes/recettes.js
@@ -1,3 +1,5 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
+const recetteController = require('../controllers/recetteController');
+
