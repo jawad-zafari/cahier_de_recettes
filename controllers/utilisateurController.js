46052f1e215583cd7d0d0fcd04170a4132d6d614
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-12 10:39:17 +0200

    Ajouter la fonction d'inscription pour les utilisateurs

--- controllers/utilisateurController.js
+++ controllers/utilisateurController.js
@@ -2,7 +2,11 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const Utilisateur = require('../models/Utilisateur');
 
-
 const CLE_SECRETE = process.env.CLE_SECRETE;
 
+
+// Inscription d'un nouvel utilisateur
+exports.inscription = async (req, res) => {
+    
+};
 
