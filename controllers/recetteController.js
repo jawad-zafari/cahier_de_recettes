d1ea722ce10107528a7fb16bdba04ae0aea85980
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-12 10:58:10 +0200

    Ajouter la fonction pour créer une nouvelle recette

--- controllers/recetteController.js
+++ controllers/recetteController.js
@@ -1,2 +1,13 @@
 const Recette = require('../models/Recette');
 
+// Créer une nouvelle recette
+exports.creerRecette = async (req, res) => {
+    try {
+        const nouvelleRecette = new Recette(req.body);
+        const recetteSauvegardee = await nouvelleRecette.save();
+        res.status(201).json(recetteSauvegardee);
+    } catch (erreur) {
+        res.status(400).json({ message: erreur.message });
+    }
+};
+
