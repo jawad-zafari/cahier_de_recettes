b8260194e447eb4cfdb23b057c750eab87b94287
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-12 10:40:38 +0200

    Ajouter la vérification de l'existence de l'utilisateur lors de l'inscription

--- controllers/utilisateurController.js
+++ controllers/utilisateurController.js
@@ -8,6 +8,11 @@ const CLE_SECRETE = process.env.CLE_SECRETE;
 // Inscription d'un nouvel utilisateur
 exports.inscription = async (req, res) => {
     try {
+        // Vérifier si l'utilisateur existe déjà
+        const utilisateurExistant = await Utilisateur.findOne({ email: req.body.email });
+        if (utilisateurExistant) {
+            return res.status(400).json({ message: 'Cet email est déjà utilisé.' });
+        }
         } catch (erreur) {
     }
 };
