0dca815ebc720ced6113ac2320838ee657a32165
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-12 10:51:42 +0200

    Ajouter la route pour l'inscription des utilisateurs

--- routes/utilisateurs.js
+++ routes/utilisateurs.js
@@ -2,3 +2,4 @@ const express = require('express');
 const router = express.Router();
 const utilisateurController = require('../controllers/utilisateurController');
 
+router.post('/inscription', utilisateurController.inscription);
