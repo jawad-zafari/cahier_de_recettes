35b4ad3d4f296d3821fcb4ab91672f0901808be5
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-12 10:58:56 +0200

    Ajouter la fonction pour récupérer toutes les recettes avec filtrage et tri

--- controllers/recetteController.js
+++ controllers/recetteController.js
@@ -11,3 +11,22 @@ exports.creerRecette = async (req, res) => {
     }
 };
 
+// Récupérer toutes les recettes avec filtrage et tri
+exports.obtenirRecettes = async (req, res) => {
+    try {
+        const filtre = {};
+        
+        // Filtrer par auteur
+        if (req.query.auteur) filtre.auteur = req.query.auteur;
+        
+        // Filtrer par ingrédient
+        if (req.query.ingredient) filtre.ingredients = req.query.ingredient;
+
+        // Trier par date (les plus récentes en premier)
+        const recettes = await Recette.find(filtre).sort({ date: -1 });
+        res.json(recettes);
+    } catch (erreur) {
+        res.status(500).json({ message: erreur.message });
+    }
+};
+
